f8c4a4c8b8d90618735073df83fbbaac2dd1fcdd
JacobLoe <Jcbl423@gmail.com>
2020-07-11 16:18:27 +0200

    script can now process a video-file

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,20 +1,21 @@
+# source $HOME/tmp/deepspeech-venv/bin/activate
+
 from deepspeech import Model
 import wave
-
+import argparse
 import numpy as np
+import subprocess
 
-ds = Model('/root/deepspeech-0.7.4-models.pbmm')
-scorer = '/root/deepspeech-0.7.4-models.scorer'
-ds.enableExternalScorer(scorer)
 
-wavefile = '/root/Bannon_Stephen_K_Generation_Zero.wav'
-
-segment_length_ms = 3000
-max_length_ms = 600000  # 10 min
+def extract_wav_from_video(video_path):
+    audio_path = '/tmp/audio.wav'
+    command = "ffmpeg -y -i {0} -ab 160k -ac 2 -ar 44100 -vn {1}".format(video_path, audio_path)
+    subprocess.call(command, shell=True)
 
 
 def metadata_to_string(metadata):
     conf = np.abs(metadata.confidence)
+    print(conf)
     if conf > 10.0:
         return ''.join(token.text for token in metadata.tokens) + " ({0})".format(conf)
     else:
@@ -54,15 +55,43 @@ def words_from_candidate_transcript(metadata):
     return word_list
 
 
-with wave.open(wavefile, 'rb') as ain:
-    framerate = ain.getframerate()
-    nframes = ain.getnframes()
-    audio_length_ms = nframes * (1. / framerate) * 1000
+def process_wavefile(ds, wavefile, segment_length_ms):
+    with wave.open(wavefile, 'rb') as ain:
+        framerate = ain.getframerate()
+        nframes = ain.getnframes()
+        audio_length_ms = nframes * (1. / framerate) * 1000
 
-    for start_ms in range(0, int(audio_length_ms), segment_length_ms):
-        ain.setpos(int(start_ms / 1000. * framerate))
-        chunkData = np.frombuffer(ain.readframes(int(segment_length_ms / 1000. * framerate)), np.int16)
+        for start_ms in range(0, int(audio_length_ms), segment_length_ms):
+            ain.setpos(int(start_ms / 1000. * framerate))
+            chunkData = np.frombuffer(ain.readframes(int(segment_length_ms / 1000. * framerate)), np.int16)
 
-        words = metadata_to_string(ds.sttWithMetadata(chunkData, 1).transcripts[0])
+            words = metadata_to_string(ds.sttWithMetadata(chunkData, 1).transcripts[0])
 
-        print("{0} {1} {2}".format(start_ms, start_ms + segment_length_ms, words))
+            print("{0} {1} {2}".format(start_ms, start_ms + segment_length_ms, words))
+
+
+def main(deepspeech_model, deepspeech_scorer, video_path, segment_length):
+    ds = Model('../deepspeech-0.7.4-models.pbmm')
+    scorer = '../deepspeech-0.7.4-models.scorer'
+    ds.enableExternalScorer(scorer)
+    #
+    # wavefile = '../audio/2830-3980-0043.wav'
+    #
+    segment_length_ms = 3000
+    max_length_ms = 600000  # 10 min
+    video_path = '../videos/unterordner/movies/Her_bluray_Szene_11_25fps.mp4'
+
+    extract_wav_from_video(video_path)
+
+    process_wavefile(ds, '/tmp/audio.wav', segment_length_ms)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('deepspeech_model', help='')
+    parser.add_argument('deepspeech_scorer', help='')
+    parser.add_argument('video_path', help='')
+    parser.add_argument('--segment_length', type=int, default=3000, help='')
+    args = parser.parse_args()
+
+    main(args.deepspeech_model, args.deepspeech_scorer, args.video_path, args.segment_length)
