811f94a59be5b8b566becfa5d307c37660a014b5
JacobLoe <Jcbl423@gmail.com>
2020-08-02 15:04:39 +0200

    improved detection of duplicate words in overlapping segments

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -11,7 +11,7 @@ import glob
 import shutil
 import hashlib
 
-VERSION = '20200722'
+VERSION = '20200731'
 
 
 def extract_wav_from_video(video_path, movie_id):
@@ -88,45 +88,56 @@ def process_wavefile(ds, wavefile, segment_length_ms, min_confidence):
             chunkData = np.frombuffer(ain.readframes(int(segment_length_ms / 1000. * framerate)), np.int16)
 
             words = words_from_candidate_transcript(ds.sttWithMetadata(chunkData, 1).transcripts[0], start_ms)
-            # words = metadata_to_string(ds.sttWithMetadata(chunkData, 1).transcripts[0], min_confidence)
-            # print(words)
 
             transcript.append((start_ms, start_ms + segment_length_ms, words))
     return transcript
 
 
-def process_transcript(ds, wavefile, transcript, overlap_length_ms, confidence):
+def get_overlapping_segments(ds, wavefile, transcript, overlap_length_ms):
     new_transcript = []
-    # print(transcript[8])
-    # print(asd)
     with wave.open(wavefile, 'rb') as ain:
         framerate = ain.getframerate()
         nframes = ain.getnframes()
         audio_length_ms = nframes * (1. / framerate) * 1000
-        for begin_segment, end_segment, segment in transcript:#[12:15]:
-            # print(segment)
+        for begin_segment, end_segment, segment in transcript:
             begin_overlap = end_segment-overlap_length_ms/2
             if begin_overlap+overlap_length_ms >= audio_length_ms:
                 break
             ain.setpos(int(begin_overlap/1000. * framerate))
             chunkData = np.frombuffer(ain.readframes(int(overlap_length_ms / 1000. * framerate)), np.int16)
             segment_overlap = words_from_candidate_transcript(ds.sttWithMetadata(chunkData, 1).transcripts[0], begin_overlap)
-            # print(segment_overlap)
-            if segment:    # check if the transcript contains any word
-                line = ' '.join(word['word'] for word in segment)    # concatenate the words in the transcript into one line
-                if segment_overlap:     # check if any words are in the overlap
-                    for so in segment_overlap:   # for every word in the overlap check if there already is a word in the current segment at the timestamp
-                        for word in segment:
-                            if so['start_time'] == word['start_time']:
-                                break
-                        line = line+' '+so['word']
-                new_transcript.append((begin_segment, end_segment, line))
-            else:
-                new_transcript.append((begin_segment, end_segment, 'SIL'))
-            # print(new_transcript[-1])
-            # print('\n')
-    #
-    # print(ads)
+
+            new_transcript.append((begin_segment, end_segment, segment))
+            new_transcript.append(segment_overlap)
+
+    return new_transcript
+
+
+def process_transcript(transcript, timestamp_threshold):
+    new_transcript = []
+    i = 0
+    while i < len(transcript):
+        # get the timestamps and words (and their timestamps) of the current segment
+        begin_segment, end_segment, segment = transcript[i]
+        if transcript[i+1]:     # check whether a overlap exists
+            ov_segment = transcript[i+1]    # get the words and timestamps from the overlap
+            line = ' '  # start an empty line
+            for s in segment:   # for every word in the segment
+                line = line+' '+s['word']   # add the current word to the line
+                for o in ov_segment:    # for every word in the overlap, check if the distance between the timestamp of two words is within the threshold
+                    if np.absolute(o['start_time'] - s['start_time']) < timestamp_threshold:
+                        if s['word'].find(o['word']) == -1 and o['word'].find(s['word']) == -1:  # if neither word is a subset of the other -> the words are completely different
+                            # append o to the line
+                            line = line + ' ' + (o['word'])  # add the current word in the overlap to the line, if the above conditions are true
+                        elif o['word'].find(s['word']) == 0 and not len(o['word']) == len(s['word']):    # if s is subset of o (o is the longer word) and starts at index 0 of o (and the words aren't the same)
+                            # replace s with o
+                            line = o['word'].join(line.rsplit(s['word'], 1))    # replace the last added word s with the word o
+                        else:   # for every other case, (o is subset of s, word indexes start anything other than 0) do nothing
+                            pass
+        else:
+            line = ' '.join(s['word'] for s in segment)     # join the words in the segment into a sentence
+        new_transcript.append((begin_segment, end_segment, line))
+        i += 2  # jump to the next segment
     return new_transcript
 
 
@@ -138,7 +149,7 @@ def write_transcript_to_file(features_path, transcript):
 
 
 def transcribe(deepspeech_model, deepspeech_scorer, list_videos_path, movie_ids,
-               features_path, segment_length_ms, overlap_length_ms, min_confidence):
+               features_path, segment_length_ms, overlap_length_ms, min_confidence, timestamp_threshold):
     ds = Model(deepspeech_model)
     ds.enableExternalScorer(deepspeech_scorer)
 
@@ -158,7 +169,8 @@ def transcribe(deepspeech_model, deepspeech_scorer, list_videos_path, movie_ids,
                 extract_wav_from_video(v_path, mid)
                 audio_path = '/tmp/' + str(mid) + '_audio.wav'
                 transcript = process_wavefile(ds, audio_path, segment_length_ms, min_confidence)
-                new_transcript = process_transcript(ds, audio_path, transcript,overlap_length_ms,  min_confidence)
+                transcript_with_overlaps = get_overlapping_segments(ds, audio_path, transcript, overlap_length_ms)
+                new_transcript = process_transcript(transcript_with_overlaps, timestamp_threshold)
                 write_transcript_to_file(f_path, new_transcript)
                 # create a hidden file to signal that the optical flow for a movie is done
                 # write the current version of the script in the file
@@ -178,24 +190,27 @@ def transcribe(deepspeech_model, deepspeech_scorer, list_videos_path, movie_ids,
                 print('optical flow was not done correctly for {}'.format(video_name))
 
 
-def main(deepspeech_model, deepspeech_scorer, videos_path, features_path, segment_length_ms, overlap_length_ms, min_confidence):
+def main(deepspeech_model, deepspeech_scorer, videos_path, features_path, segment_length_ms, overlap_length_ms, min_confidence, timestamp_threshold):
     list_videos_path = glob.glob(os.path.join(videos_path, '**/*.mp4'), recursive=True)  # get the list of videos in videos_dir
 
     movie_ids = [hashlib.sha256(os.path.split(v)[-1][:-4].encode('utf8')).hexdigest() for v in list_videos_path]
 
-    transcribe(deepspeech_model, deepspeech_scorer, list_videos_path, movie_ids, features_path, segment_length_ms, overlap_length_ms, min_confidence)
+    transcribe(deepspeech_model, deepspeech_scorer, list_videos_path, movie_ids, features_path, segment_length_ms, overlap_length_ms, min_confidence, timestamp_threshold)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('videos_path', help='path to the video to be transcribed')
     parser.add_argument('features_path', help='path to the directory where the results are saved')
-    parser.add_argument('--deepspeech_model', default='../deepspeech-0.7.4-models.pbmm', help='path to a deepspeech model')
-    parser.add_argument('--deepspeech_scorer', default='../deepspeech-0.7.4-models.scorer', help='path to a deepspeech scorer')
-    parser.add_argument('--segment_length_ms', type=int, default=3000, help='')
-    parser.add_argument('--overlap_length_ms', type=int, default=200, help='')
-    parser.add_argument('--min_confidence', type=float, default=10.0, help='')
+    parser.add_argument('--deepspeech_model', default='../deepspeech-0.7.4-models.pbmm', help='path to a deepspeech model, default is ../deepspeech-0.7.4-models.pbmm')
+    parser.add_argument('--deepspeech_scorer', default='../deepspeech-0.7.4-models.scorer', help='path to a deepspeech scorer, default is ../deepspeech-0.7.4-models.scorer')
+    parser.add_argument('--segment_length_ms', type=int, default=3000, help='lenght of the segments for which audio is transcribed, in milliseconds, default 3000')
+    parser.add_argument('--overlap_length_ms', type=int, default=100, help='lenght of the overlap between two segments, in milliseconds, default ')
+    parser.add_argument('--min_confidence', type=float, default=10.0, help='minimum confidence the deepspeech model has to return for a word'
+                                                                           'if the confidence is lower SIL is returned instead, default is 10.0')
+    parser.add_argument('--timestamp_threshold', type=int, default=50, help='maximum number of milliseconds two timestamps can be apart of each other to be considered the same, '
+                                                                            'in milliseconds, default')
     args = parser.parse_args()
 
     main(args.deepspeech_model, args.deepspeech_scorer, args.videos_path, args.features_path,
-         args.segment_length_ms, args.overlap_length_ms, args.min_confidence)
+         args.segment_length_ms, args.overlap_length_ms, args.min_confidence, args.timestamp_threshold)
